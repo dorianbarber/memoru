61e6378d1e37afa0e481937832bf0f2e61a02951
Dorian Barber <thedorianbarber@gmail.com>
2021-06-19 11:34:54 -0400

    post new item function added not tested tho

--- firebase/posts.js
+++ firebase/posts.js
@@ -1,5 +1,7 @@
 import { db } from '../firebase';
 
+const errorLine = 'No account storage located';
+
 function postNewTemplate( user, templateList, title) {
   const userTemplateDB = db.collection('users').doc(user).collection('template');
 
@@ -12,11 +14,29 @@ function postNewTemplate( user, templateList, title) {
     })
     .catch( (error) => {
       console.log(error);
-    })
+    });
   } else {
-    console.log('No account storage located');
+    console.log(errorLine);
   }
 }
 
+function postNewItem( user, databaseName, itemName, fields) {
+  const userItemDB = db.collection('users').doc(user).collection(databaseName);
+
+  if (userItemDB) {
+    userItemDB.doc(`${itemName}`).set({fields})
+    .then( () => {
+      console.log('new item added');
+      console.log(itemName);
+      console.log(fields);
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+  } else {
+    console.log(errorLine);
+  }
+}
 
 export default postNewTemplate;
+export default postNewItem;
