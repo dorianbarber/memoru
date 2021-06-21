79a6bed01619277521618145b2518b961b020809
Dorian Barber <thedorianbarber@gmail.com>
2021-06-21 15:45:56 -0400

    clean up bulleted list input

--- components/inputs/BulletedListInput.js
+++ components/inputs/BulletedListInput.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TextInput, StyleSheet, Button } from 'react-native';
-
-function findFirstDiff(a, b) {
-  var i = 0;
-  if (a === b) return -1;
-  while (a[i] === b[i]) i++;
-  return i;
-}
+import { View, Text, FlatList, TextInput, StyleSheet } from 'react-native';
 
 // each line is a view pair 
 // each view pair has 1 Text and 1 Text input
@@ -96,43 +89,5 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   }
 });
-
-// function BulletedListInput({ placeholder }) {
-//   const [text, setText] = useState('')
-
-//   const onChangeText = (newValue) => {
-//     const newCharIndex = findFirstDiff(text, newValue);
-//     if (newValue[newCharIndex] === '\n') {
-//       const correctValue = newValue.slice(0, newCharIndex + 1) + '\u2022 ' + newValue.slice(newCharIndex + 1);
-//       setText(correctValue);
-//     } else {
-//       setText(newValue);
-//     }
-//   };
-
-//   const onEmptyFocus = () => {
-//     if (text === '') {
-//       setText('\u2022 ');
-//     }
-//   };
-
-//   const handleEnterKey = (e) => {
-//     if (e.nativeEvent.key === 'Enter') {
-//       const newText = text.concat('\u2022 ');
-//       setText(newText);
-//     }
-//   }
-
-//   return (
-//     <TextInput
-//       onFocus={onEmptyFocus}
-//       onKeyPress={handleEnterKey}
-//       onChangeText={onChangeText}
-//       value={text}
-//       multiline={true}
-//       placeholder={placeholder}
-//     />
-//   );
-// }
 
 export default BulletedListInput;
