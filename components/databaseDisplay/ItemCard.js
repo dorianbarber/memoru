a74867d6d1082c5dc83aa2aedc88b0f77eacde67
Dorian Barber <thedorianbarber@gmail.com>
2021-06-22 18:48:56 -0400

    working on styling

--- components/databaseDisplay/ItemCard.js
+++ components/databaseDisplay/ItemCard.js
@@ -14,8 +14,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     elevation: 2,
-    width: '100%',
-    height: '20%',
+    height: 100,
+    margin: 10,
+    padding: 35,
+    borderRadius: 20,
+    backgroundColor: 'white',
   },
   itemId: {
     fontSize: 20,
