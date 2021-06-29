80356ad0118efad2f19b11911e54d90936c9cecc
Dorian Barber <thedorianbarber@gmail.com>
2021-06-29 14:34:13 -0400

    firebase get and call in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 import HomeHeader from '../components/headers/HomeHeader.js';
 import HomeActionButton from '../components/buttons/HomeActionButton.js';
 
+import { getAllTemplates } from '../firebase/gets';
+
 function HomeScreen({ navigation }) {
   const [optionsActive, setOptionsActive] = useState(false)
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    async function fetchTemplates() {
+      const response = await getAllTemplates('userTest');
+      let freshData = [];
+      response.forEach(doc => {
+        freshData.push(doc.id);
+      });
+      setTemplates(freshData);
+    };
+    fetchTemplates();
+  });
 
   return (
     <View style={styles.container}>
